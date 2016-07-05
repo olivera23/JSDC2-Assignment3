c962ac890534cfb1e9967caae278a48e1af08512
Arie Olivera <arieolivera23@outlook.com>
2016-07-05 15:02:03 -0400

    sample function test

---
+++ sampleJSProject.js
@@ -0,0 +1,12 @@
+
+       function showMessage(m, string, num){
+           console.log(m + string + num);
+           
+       }
+       
+       var month = "March"
+       
+       
+       showMessage(month, "'s winner number is ", 23);
+       
+       
